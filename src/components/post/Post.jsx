3e0801bba02bbd4792c527e38b1f820b0b9c9fdc
shift1018 <105242991+shift1018@users.noreply.github.com>
2022-11-22 13:17:37 -0500

    r

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,37 @@
+import "./post.css";
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import {Users} from "../../data.js"
+
+export default function Post({post}) {
+    return(
+        <div className="post">
+            <div className="postWrapper">
+                <div className="postTop">
+                    <div className="postTopLeft">
+                        <img src={Users.filter((u)=> u.id === post.userId)[0].profilePicture} alt="" className="postProfileImg" />
+                        <span className="postUserName">{Users.filter((u)=> u.id === post.userId)[0].username}</span>
+                        <span className="postDate"> {post.date}</span>
+                    </div>
+                    <div className="posTopRight">
+                   < MoreHorizIcon/>
+                    </div>
+                </div>
+                <div className="postCenter">
+                    <span className="postText">{post?.desc}</span>
+                    <img src={post.photo} alt="" className="postImg" />
+                </div>
+                <div className="postBottom">
+                    <div className="postBottomLeft">
+                        <img src="/assets/post/like.png" alt="" className="likeIcon" />
+                        <img src="/assets/post/heart.png" alt="" className="likeIcon" />
+                        <span className="postLikeCounter">{post.like}</span>
+                    </div>
+                    <div className="postBottomRight">
+                        <span className="postCommentText">{post.comment}comments</span>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+};
