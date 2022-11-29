dfc79c88fe62ed1aac002162b9d50cdb3c120472
shift1018 <105242991+shift1018@users.noreply.github.com>
2022-11-29 09:20:56 -0500

    r

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -102,15 +102,16 @@ export default function Post({ post }) {
             />
             <span className="postLikeCounter">{like}</span>
           </div>
-          <div className="postBottomRight">
+          {/* <div className="postBottomRight">
             <span className="postCommentText">{post.comment}comments</span>
-          </div>
-          <Comments
+          </div> */}
+          
+        </div>
+        <Comments
             CommentLists={CommentLists}
             postId={post._id}
             refreshFunction={updateComment}
           />
-        </div>
       </div>
     </div>
   );
