ff3472eab908c973bca814f01765bf0885e49e8f
shift1018 <105242991+shift1018@users.noreply.github.com>
2022-12-08 02:42:59 -0500

    r

--- src/components/post/EditPost.jsx
+++ src/components/post/EditPost.jsx
@@ -2,12 +2,35 @@ import React, { useEffect, useState, useContext } from "react";
 import axios from "../../utils/axios";
 import { useParams } from "react-router";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useNavigate } from "react-router-dom";
+
+
+
+
 
 export default function EditPost() {
   const [post, setPost] = useState({});
   const [content, setContent] = useState();
   const { postId } = useParams();
   const [error, setError] = useState("");
+  let navigate = useNavigate();
+
+
+  const [fileSelected, setFileSelected] = useState([]);
+
+  const onFileChange = (event) => {
+    // capture file into state
+    setFileSelected(event.target.files);
+  };
+  const formData = new FormData()
+
+
+  formData.append('title', "default title");
+    
+  formData.append('content', content);
+  for( var i =0; i< fileSelected.length;i++){
+    formData.append("fileName", fileSelected[i])
+  }
 
 
 
@@ -18,19 +41,22 @@ export default function EditPost() {
       console.log("Update res", res.data);
       setContent(res.data.content);
       setPost(res.data);
-    };
+    }
     fetchPost();
+
+    
     // console.log("Update content", post.content);
   }, [postId]);
 
+
+  
+
   const updatePost = (postId) => {
+   
     axios
       .patch(
         `api/posts/${postId}`,
-        {
-          title: "default title",
-          content: content,
-        },
+        formData,
         {
           headers: { accessToken: localStorage.getItem("accessToken") },
         }
@@ -42,21 +68,25 @@ export default function EditPost() {
           setPost(response.data);
           window.location.reload();
         }
+        const urlList = post.imageURL;
+        if (urlList.length > 0 && urlList[0] != "") {
+        // divBool=true;
+        const items =  urlList.map((value, key) => {
+          return <div className="postPhotoGridItem"><img className="photoImg" src={value} alt="" /></div>
+          {/* // return </div> */}
+        }
+        );
+      } else {
+        var items = <br></br>;
+      }
       });
+
+      navigate("/");
+      
   };
 
 
-  const urlList = post.imageURL;
-  if (urlList.length > 0 && urlList[0] != "") {
-    // divBool=true;
-    var items =  urlList.map((value, key) => {
-      return <div className="postPhotoGridItem"><img className="photoImg" src={value} alt="" /></div>
-      {/* // return </div> */}
-    }
-    );
-  } else {
-    var items = <br></br>;
-  }
+  
 
   return (
     <div>
@@ -83,9 +113,11 @@ export default function EditPost() {
               />
             </td>
             <td>
-            <div className="postPhotoGrid">{items} <br /></div>
+            <div className="postPhotoGrid">
+              {/* {items}  */}
+              <br /></div>
             <input type="file"  accept="image/jpeg, image/png, image/jpg"  multiple="multiple" 
-            // onChange={onFileChange} 
+            onChange={onFileChange} 
             />
               <button
                 onClick={() => {
