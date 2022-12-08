98e6fe81cee816c591771fc9d5b70d69c8290f08
shift1018 <105242991+shift1018@users.noreply.github.com>
2022-12-08 00:41:53 -0500

    r

--- src/utils/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  // baseURL: "http://localhost:8800/",
-  baseURL: "https://ticklebackend.herokuapp.com/",
-});
-
-
-export default instance;
